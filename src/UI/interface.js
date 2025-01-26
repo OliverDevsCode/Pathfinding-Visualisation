e29afc73380b2c3140c64032b2c61cb44542f124
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2025-01-26 21:00:12 +0000

    Cleaning Up Code

--- src/UI/interface.js
+++ src/UI/interface.js
@@ -13,7 +13,6 @@ let resetButton;
 let pathText;
 let costText;
 
-
 let graph = new Graph(); //called graph for automated testing
 
 let options = ["Dijkstra","A*"]
@@ -127,4 +126,37 @@ function reset(){
   startSelect = createSelect()
   goalSelect = createSelect()
   resetButton = createButton("Reset")
+}
+
+function doubleClicked(){
+  if(mode == "Link Node"){
+      console.log("Node Clicked")
+      //calculate if node pressed
+      for(let i = 0; i < nodes.length;i++){
+        range = dist(mouseX,mouseY,nodes[i].x,nodes[i].y)
+        if(range < 50){
+          console.log("In Range")
+          nodesToLink.push(nodes[i])
+          clicks ++
+          if(clicks > 1){
+            linkNodes(nodesToLink)
+            console.log("Mode Reset")
+            mode = "view"
+            clicks = 0
+          }
+        }
+      }
+  }
+  
+  if(mode == "Add Node"){
+    let num = nodes.length + 1
+    let node = new Node(mouseX,mouseY,num)
+    nodes.push(node)
+    startSelect.option(node.num)
+    goalSelect.option(node.num)
+    graph.addNode(node)
+    console.log("Node Added")
+    mode = "view"
+    console.log("Mode Reset")
+  }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,6 @@
 function setup() {
   createCanvas(800, 800);
-
   createUI()
-
-
 }
 
 function draw() {
@@ -13,40 +10,5 @@ function draw() {
   linkButton.mousePressed(setModeLink)
   solveButton.mousePressed(solveGraph)
   resetButton.mousePressed(reset)
-}
-
-function doubleClicked(){
-
-  if(mode == "Link Node"){
-      console.log("Node Clicked")
-      //calculate if node pressed
-      for(let i = 0; i < nodes.length;i++){
-        range = dist(mouseX,mouseY,nodes[i].x,nodes[i].y)
-        if(range < 50){
-          console.log("In Range")
-          nodesToLink.push(nodes[i])
-          clicks ++
-          if(clicks > 1){
-            linkNodes(nodesToLink)
-            console.log("Mode Reset")
-            mode = "view"
-            clicks = 0
-          }
-        }
-      }
-    
-  }
-  
-  if(mode == "Add Node"){
-    let num = nodes.length + 1
-    let node = new Node(mouseX,mouseY,num)
-    nodes.push(node)
-    startSelect.option(node.num)
-    goalSelect.option(node.num)
-    graph.addNode(node)
-    console.log("Node Added")
-    mode = "view"
-    console.log("Mode Reset")
-  }
-  
+  exportButton.mousePressed(exportGraph)
 }
